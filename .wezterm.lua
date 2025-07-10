6cf2df73976c3738076884eda659d0ff67b38b17
Lars Loonen <lars.van.der.spek@vandoren.nl>
2025-07-10 10:44:37 +0200

    feat: added move tab

--- .wezterm.lua
+++ .wezterm.lua
@@ -194,6 +194,15 @@ config.keys = {
   },
 }
 
+for i = 1, 8 do
+  -- CTRL+ALT + number to move to that position
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL|ALT',
+    action = wezterm.action.MoveTab(i - 1),
+  })
+end
+
 config.unix_domains = {
   {
     name = 'unix',
